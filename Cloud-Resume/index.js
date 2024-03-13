e95b36993b25de4b90dee88816b28cabbb0314db
Ganga Gautam <gangagautam59@gmail.com>
2024-03-13 18:39:17 -0500

    view change

--- Cloud-Resume/index.js
+++ Cloud-Resume/index.js
@@ -2,6 +2,6 @@ const counter = document.querySelector(".counter-number");
 async function updateCounter() {
         let response = await fetch("https://hxfl29flw2.execute-api.us-east-1.amazonaws.com/Prod/visitor/");
         let data = await response.json();
-        counter.innerHTML = ` Views: ${data}`;
+        counter.innerHTML = ` ${data}`;
 }
 updateCounter();
